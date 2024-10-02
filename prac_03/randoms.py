2e9bc0bc2ea7100ea59a320e5552c3b98e8fe53b
Jake <jake.dodgson@my.jcu.edu.au>
2024-10-02 21:13:29 +1000

    add all prac_03 files

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+#
+#Q: What did you see on line 1?
+#Q: What was the smallest number you could have seen, what was the largest?
+#A: I saw 10, Smallest number possible: 5.  The largest number: 20.
+
+#Q: What did you see on line 2?
+#Q: What was the smallest number you could have seen, what was the largest?
+#Q: Could line 2 have produced a 4?
+#A:I saw 5, Smallest number possible: 3. Largest number possible: 9. no it could not have produced a 4.
+
+#Q: What did you see on line 3?
+#Q: What was the smallest number you could have seen, what was the largest?
+#A:I saw 3.5835445162767052, Smallest number possible: 2.5. Largest number possible: 5.4999999999
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+
+import random
+print(random.randint(1, 100))
