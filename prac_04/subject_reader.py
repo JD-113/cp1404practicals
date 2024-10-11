aa38bcbdd861996aef420758b2f967eb438c9218
Jake <jake.dodgson@my.jcu.edu.au>
2024-10-11 19:16:11 +1000

    Add load_data function change to load_subjects made to return nested list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,29 +1,36 @@
 """
 CP1404/CP5632 Practical
 Data file -> lists program
+
+
 """
 
-FILENAME = "subject_data.txt.txt"
+# print(line)  # See what a line looks like
+# print(repr(line))  # See what a line really looks like
+# line = line.strip()  # Remove the \n
+# parts = line.split(',')  # Separate the data into its parts
+# print(parts)  # See what the parts look like (notice the integer is a string)
+# parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+# print(parts)  # See if that worked
+
+FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
+    data = load_subjects()
     print(data)
 
 
-def load_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+def load_subjects():
+    subject = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject.append(parts)
     input_file.close()
+    return subject
 
 
 main()
