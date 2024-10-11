da08909e97db53b6572883602dc196af9730a8c6
Jake <jake.dodgson@my.jcu.edu.au>
2024-10-11 18:14:16 +1000

    Add statements answers

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,4 +1,6 @@
+"""I've left commented out due to warnings"""
 numbers = [3, 1, 4, 1, 5, 9, 2]
+
 # numbers[0] = 3
 # numbers[-1] = 2
 # numbers[3] = 1
@@ -8,3 +10,8 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers = False
 # "3" in numbers = False
 # numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# 1. numbers[0] = "ten"
+# 2. numbers[-1] = 1
+# 3. numbers[2:]
+# 4. 9 in numbers
