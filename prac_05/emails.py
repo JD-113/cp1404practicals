31670564d8bf4082839548a1955b1af4e2d7465e
Jake <jake.dodgson@my.jcu.edu.au>
2024-10-20 10:57:17 +1000

    Complete prac 5 emails exercise

---
+++ prac_05/emails.py
@@ -0,0 +1,28 @@
+
+
+
+def main():
+    email_to_name = {}
+    email = input('Email: ')
+    while email != '':
+        name = extract_name_from_email(email)
+        affirmation = input(f"Is your name {name}? (Y/n)")
+        if affirmation.upper() != "Y" and affirmation.upper() != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input('Email: ')
+
+    for email, name in email_to_name.items():
+        print(name, email)
+
+    pass
+
+
+def extract_name_from_email(email):
+    name_part = email.split('@')[0]
+    name_parts = name_part.split('.')
+    name = ".".join(name_parts).title()
+    return name
+
+
+main()
