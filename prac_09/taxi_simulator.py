f97fb3630b6a6f0f25b82ff6e5fb7419ebce9754
Jake <jake.dodgson@my.jcu.edu.au>
2024-11-23 21:18:01 +1000

    Complete prac 9 taxi simulator exercise done

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,61 @@
+"""CP1404 Prac_09 taxi simulator."""
+
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    """Taxi simulator program."""
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    total_bill = 0
+    current_taxi = None
+    print("Let's drive!")
+    print(MENU)
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            print("Taxis available: ")
+            display_taxis(taxis)
+            current_taxi = return_valid_taxi_number(taxis)
+        elif menu_choice == "d":
+            if current_taxi:
+                current_taxi.start_fare()
+                distance_to_drive = float(input("Drive how far? "))
+                current_taxi.drive(distance_to_drive)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+                total_bill += trip_cost
+            else:
+                print("You need to choose a taxi before you can drive")
+        else:
+            print("Invalid option")
+        print(f"Bill to date: ${total_bill:.2f}")
+        print(MENU)
+        menu_choice = input(">>> ").lower()
+    print(f"Total trip cost: ${total_bill:.2f}")
+    print("Taxis are now:")
+    display_taxis(taxis)
+
+
+def display_taxis(taxis):
+    """Display the list of taxis"""
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+def return_valid_taxi_number(taxis):
+    """Get a valid taxi number"""
+    try:
+        taxi_choice = int(input("Choose taxi: "))
+        if 0 <= taxi_choice < len(taxis):
+            return taxis[taxi_choice]
+        else:
+            print("Invalid taxi choice")
+    except ValueError:
+        print("Invalid input")
+
+
+main()
