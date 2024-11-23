1ddb5aff91f0d4f512c1583e6a3024272238218a
Jake <jake.dodgson@my.jcu.edu.au>
2024-11-23 19:25:06 +1000

    Complete prac 9 Test Taxi done

---
+++ prac_09/car.py
@@ -0,0 +1,28 @@
+"""CP1404 Prac Car class."""
+
+
+class Car:
+    """Represent a Car object."""
+
+    def __init__(self, name="", fuel=0):
+        """Initialise a Car instance"""
+        self.fuel = fuel
+        self._odometer = 0
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
+
+    def add_fuel(self, amount):
+        """Add amount to the car's fuel."""
+        self.fuel += amount
+
+    def drive(self, distance):
+        """Drive the car a given distance."""
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self._odometer += distance
+        return distance
