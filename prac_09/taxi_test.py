cb11ea7ea96c5f08264e74432f566f55ac50e2d8
Jake <jake.dodgson@my.jcu.edu.au>
2024-11-23 19:23:12 +1000

    Complete prac 9 Test Taxi done

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,13 @@
+"""CP1404 Prac Taxi test"""
+
+from prac_09.taxi import Taxi
+
+
+def main():
+    """Test for Taxi Class"""
+    my_taxi = Taxi("Prius 1", 100)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
